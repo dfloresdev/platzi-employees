125a5f3547cb444207041aab486562fcc0a57a1c
dfloresdev <hello@dflores.dev>
2020-04-21 07:16:03 -0500

    [INIT] structure ready

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../assets/styles/components/header/Header.scss";
+
+const Header = () => (
+  <>
+    <div>HOla soy menu</div>
+  </>
+);
+
+export default Header;
---
+++ src/containers/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../assets/styles/App.css";
+import Header from "../components/header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
