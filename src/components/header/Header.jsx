459b4dd6d1a123115a041c5b35469674f44eaa68
dfloresdev <hello@dflores.dev>
2020-04-21 07:26:24 -0500

    [SECTION] header ready

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,9 +2,26 @@ import React from "react";
 import "../../assets/styles/components/header/Header.scss";
 
 const Header = () => (
-  <>
-    <div>HOla soy menu</div>
-  </>
+  <div className="menu">
+    <label for="toggle">&#9776;</label>
+    <input id="toggle" type="checkbox" />
+    <div className="menu--items">
+      <a href="#" rel="noopener noreferrer">
+        <p>Home</p>
+      </a>
+      <a href="#" rel="noopener noreferrer">
+        <p>Shot</p>
+      </a>
+      <a href="#" rel="noopener noreferrer">
+        <p>Profile</p>
+      </a>
+      <a></a>
+      <a></a>
+    </div>
+    <div className="menu--userName">
+      <p>Username</p>
+    </div>
+  </div>
 );
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./assets/styles/index.css";
+import "./assets/styles/index.scss";
 import App from "./containers/App";
 import * as serviceWorker from "./serviceWorker";
 
