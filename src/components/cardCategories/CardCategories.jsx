d9472964dc697de4f7eaa27dafc6773044d50b20
dfloresdev <hello@dflores.dev>
2020-05-04 02:53:16 -0500

    [CARD-CATEGORIES] 50% ready

---
+++ src/components/cardCategories/CardCategories.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../assets/styles/components/cardCategories/CardCategories.scss";
+
+const CardCategories = () => (
+  <div className="container-card-categories">
+    <p>Todos los empleados</p>
+    <p>Proyecto</p>
+    <p className="category active">Deportes🧸</p>
+    <p className="category">Deportes🧸</p>
+  </div>
+);
+
+export default CardCategories;
--- src/containers/Dashboard.jsx
+++ src/containers/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../assets/styles/components/dashboard/Dashboard.scss";
 import ListEmployees from "../components/listEmployees/ListEmployees";
 import SubHeader from "../components/subHeader/SubHeader";
+import CardCategories from "../components/cardCategories/CardCategories";
 
 const Dashboard = () => {
   const handleSubmit = (event) => {
@@ -13,7 +14,9 @@ const Dashboard = () => {
       <div className="sub-header">
         <SubHeader />
       </div>
-      <div className="">vertical</div>
+      <div className="">
+        <CardCategories />
+      </div>
       <div className="">
         <ListEmployees />
       </div>
