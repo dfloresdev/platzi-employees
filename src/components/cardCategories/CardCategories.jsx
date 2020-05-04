fb1192667d1dafc4daf1bb6e40452db57784c79f
dfloresdev <hello@dflores.dev>
2020-05-04 17:27:40 -0500

    [CARD-CATEGORIES] section ready

--- src/components/cardCategories/CardCategories.jsx
+++ src/components/cardCategories/CardCategories.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "../../assets/styles/components/cardCategories/CardCategories.scss";
+import Icons from "../../assets/styles/components/icons/svgIcons";
 
 const CardCategories = () => (
   <div className="container-card-categories">
-    <p>Todos los empleados</p>
-    <p>Proyecto</p>
-    <p className="category active">Deportes🧸</p>
-    <p className="category">Deportes🧸</p>
+    <div className="container-card-categories--header">
+      <Icons name="success" fill="#cd7cf2" /> <p>Todos los empleados</p>
+    </div>
+    <p className="container-card-categories--description">Proyecto</p>
+    <p className="category active">Deportes 🧸</p>
+    <p className="category">Automóviles 🏎</p>
+    <p className="category">Construcción 🛠</p>
+    <p className="category">Programación 💻</p>
+    <p className="category">Animales 🐶</p>
   </div>
 );
 
