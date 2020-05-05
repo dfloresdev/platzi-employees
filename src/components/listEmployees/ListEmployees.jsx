b5f776fbfc07f8c303e01ced475ab9331d47bf3d
dfloresdev <hello@dflores.dev>
2020-05-04 19:00:03 -0500

    [LIST-EMPLOYEES] add cards with info

--- src/components/listEmployees/ListEmployees.jsx
+++ src/components/listEmployees/ListEmployees.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../assets/styles/components/listEmployees/ListEmployees.scss";
+import Icons from "../../assets/styles/components/icons/svgIcons";
 import useGetEmployees from "../../hooks/useGetEmployees";
 
 const endpoint = "employee";
@@ -8,36 +9,48 @@ const ListEmployees = () => {
   const employees = useGetEmployees(endpoint);
 
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th></th>
-            <th>Empleado</th>
-            <th>Salario</th>
-            <th>Estado</th>
-            <th>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {employees.map((employee) => {
-            return (
-              <tr>
-                <td>
-                  <img src={employee.url_img} height="50px" />
-                </td>
-                <td>
+    <>
+      <div className="container-list-employees">
+        <div className="container-list-employees--checkbox">
+          <input type="checkbox" className="box"></input>
+        </div>
+        <p className="container-list-employees--title employee">Empleado</p>
+        <p className="container-list-employees--title">Salario</p>
+        <p className="container-list-employees--title">Estado</p>
+        <p className="container-list-employees--title">Acciones</p>
+      </div>
+      <div>
+        {employees.map((employee) => {
+          return (
+            <div className="card-employee">
+              <div>
+                <div className="container-list-employees--checkbox">
+                  <input type="checkbox" className="box"></input>
+                </div>
+              </div>
+              <div>
+                <img src="" alt="" />
+                <p>
                   {employee.nombre} {employee.apellido}
-                </td>
-                <td>{employee.salario}</td>
-                <td>{employee.estado}</td>
-                <td></td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+                </p>
+                <p>Puesto</p>
+              </div>
+              <div>
+                <p>{employee.salario} USD</p>
+                <p>full time</p>
+              </div>
+              <div>
+                <p>{employee.estado}</p>
+              </div>
+              <div>
+                <Icons name="edit" className="actions" />
+                <Icons name="delete" className="actions" />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
