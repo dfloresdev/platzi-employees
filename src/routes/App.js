eed2bc32e3eb060cbe4180e8db6eadbe7a168f2b
dfloresdev <hello@dflores.dev>
2020-05-11 12:17:25 -0500

    [LOGIN] protect routes

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,10 +1,20 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Home from "../containers/Home";
 import Login from "../containers/Login/Login";
 import NotFound from "../containers/NotFound";
 import Layout from "../components/layout/Layout";
 import Dashboard from "../containers/Dashboard/Dashboard";
+import { checkPropTypes } from "prop-types";
+
+function SecureRoutes({ component, path, ...restElements }) {
+  const storage = JSON.parse(localStorage.getItem("storage"));
+  if (storage && storage.user) {
+    return <Route path={path} component={component} {...restElements} />;
+  } else {
+    return <Redirect to="/login" {...restElements} />;
+  }
+}
 
 const App = () => (
   <BrowserRouter>
@@ -12,7 +22,7 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
-        <Route exact path="/dashboard" component={Dashboard} />
+        <SecureRoutes exact path="/dashboard" component={Dashboard} />
         <Route component={NotFound} />
       </Switch>
     </Layout>
